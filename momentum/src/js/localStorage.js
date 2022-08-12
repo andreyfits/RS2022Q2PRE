0ab3285f5bfb9f2d7331b80ad7ebce32699a196c
Andrei Fits <andreyfits@gmail.com>
2022-08-12 14:46:10 +0300

    feat: implement function for set city to localStorage

--- momentum/src/js/localStorage.js
+++ momentum/src/js/localStorage.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-undef */
+import getWeather from './weather';
+
 export default () => {
-    window.addEventListener('beforeunload', () => setNameToLocalStorage);
+    window.addEventListener('beforeunload', () => {
+        setNameToLocalStorage();
+        setCity();
+    });
 
     function setNameToLocalStorage() {
         const name = document.querySelector('.main__name');
@@ -14,5 +19,36 @@ export default () => {
         });
     }
 
+    function setCity() {
+        const city = document.querySelector('.city');
+
+        city.addEventListener('blur', () => {
+            const data = getWeather(city.value);
+
+            data.then((item) => {
+                localStorage.setItem('errorText', document.querySelector('.header__weather-error').textContent);
+                localStorage.setItem('cod', item.cod);
+                localStorage.setItem('temp', Math.floor(item.main.temp));
+                localStorage.setItem('desc', item.weather[0].description);
+                localStorage.setItem('wind', Math.floor(item.wind.speed));
+                localStorage.setItem('hum', item.main.humidity);
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', () => {
+            city.value = localStorage.getItem('city');
+            getWeather(city.value);
+        });
+
+        city.addEventListener('focus', () => {
+            localStorage.setItem('city', city.value);
+
+            city.addEventListener('keyup', () => {
+                localStorage.setItem('city', city.value);
+            });
+        });
+    }
+
     setNameToLocalStorage();
+    setCity();
 };
