2587286cbcd99b6d31ccdd6a50d59355cb09ede1
Andrei Fits <andreyfits@gmail.com>
2022-08-16 21:01:01 +0300

    feat: add playlist

---
+++ momentum/src/js/playlist.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title: 'Camila Cabello feat. Ed Sheeran - Bam Bam',
+        src: '../assets/sounds/Camila Cabello feat. Ed Sheeran - Bam Bam.mp3',
+        duration: '03:26'
+    },
+    {
+        title: 'Charli XCX feat. Rina Sawayama - Beg For You (feat. Rina Sawayama)',
+        src: '../assets/sounds/Charli XCX feat. Rina Sawayama - Beg For You (feat. Rina Sawayama).mp3',
+        duration: '02:48'
+    },
+    {
+        title: 'Ed Sheeran - 2step',
+        src: '../assets/sounds/Ed Sheeran - 2step.mp3',
+        duration: '02:33'
+    },
+    {
+        title: 'Lizzo - About Damn Time',
+        src: '../assets/sounds/Lizzo - About Damn Time.mp3',
+        duration: '03:08'
+    },
+    {
+        title: 'Nathan Dawe - 21 Reasons (feat. Ella Henderson)',
+        src: '../assets/sounds/Nathan Dawe - 21 Reasons (feat. Ella Henderson).mp3',
+        duration: '02:35'
+    },
+    {
+        title: 'Tiesto & Ava Max - The Motto',
+        src: '../assets/sounds/Tiesto & Ava Max - The Motto.mp3',
+        duration: '03:33'
+    }
+];
